eb71e4642d0818c5c4dea6c9fa19d4771e002539
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-02 17:17:58 +0100

    feat: btn

--- react-use-state/src/components/AppMain.jsx
+++ react-use-state/src/components/AppMain.jsx
@@ -1,10 +1,19 @@
 import './AppMain.css';
-
+import { languages } from '../assets/languages';
 export default function AppMain(){
     return(
         <>
             <main>
-                
+
+                <div className='flex between'>
+                {languages.map((language, i) =>(
+                    
+                        <div key={i}>
+                            <span className='btn'>{language.title}</span>
+                        </div>
+                    
+                ))} 
+                </div>
             </main>
         </>
     );
